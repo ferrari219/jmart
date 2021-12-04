86aaafda4d0a0799301f3a1204d2efaf18aaaad6
ferrari219@nate.com <ferrari219@nate.com>
2021-12-04 10:53:59 +0900

    storybook, emotion, theme 설정

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Detail from 'routers/Detail';
 import Home from 'routers/Home';
+import GlobalStyle from 'styles/GlobalStyle';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:id" element={<Detail />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    main: '#0276F9',
+    basic: '#666',
+  },
+};
+
+export default theme;
